b7aa33f5d9ee4155e1c9a50d41f9b4176ab7867b
PetersAcct <115116670+PetersAcct@users.noreply.github.com>
2022-10-07 16:30:51 -0500

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,10 @@
+mkdir $BUILD_SOURCESDIRECTORY/_dl
+mkdir $BUILD_SOURCESDIRECTORY/_bin
+mkdir $BUILD_SOURCESDIRECTORY/_publish
 
+wget -O $BUILD_SOURCESDIRECTORY/_dl/docfx.zip "https://github.com/dotnet/docfx/releases/download/v2.37/docfx.zip"
+unzip $BUILD_SOURCESDIRECTORY/_dl/docfx.zip -d $BUILD_SOURCESDIRECTORY/_bin/docfx
+
+cd $BUILD_SOURCESDIRECTORY/$DOCSFOLDER
+mono $BUILD_SOURCESDIRECTORY/_bin/docfx/docfx.exe
+cp -R $BUILD_SOURCESDIRECTORY/$DOCSFOLDER/_site/. $BUILD_SOURCESDIRECTORY/_publish/
